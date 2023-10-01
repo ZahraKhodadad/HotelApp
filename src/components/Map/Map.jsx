2b7a43febf2914e67dd426d4c9e6281ffa0323ce
zahrakhodadad <zahra.khodadad@gmail.com>
2023-10-01 16:48:13 +0330

    set map center

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,18 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../Context/HotelsProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const Map = () => {
   const { isLoading, hotels } = useHotels();
   const [mapCenter, setMapCenter] = useState([51, 3]);
-  // console.log(mapCenter);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const len = searchParams.get("len");
+  useEffect(() => {
+    if (lat & len) setMapCenter([lat, len]);
+  }, [lat, len]);
+  console.log(lat, len);
   return (
     <div className="mapContainer">
       <MapContainer
@@ -13,11 +21,12 @@ const Map = () => {
         zoom={13}
         scrollWheelZoom={true}
       >
+        <button className="getLocation">Use your Location</button>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-       
+        <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
             <Popup>{item.host_location}</Popup>
@@ -29,3 +38,9 @@ const Map = () => {
 };
 
 export default Map;
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
---
+++ src/components/hooks/useGeoLocation.jsx
@@ -0,0 +1,5 @@
+const useGeoLocation = () => {
+      return (  );
+}
+ 
+export default useGeoLocation;
